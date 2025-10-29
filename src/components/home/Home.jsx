db48be74075a8f480b1bcc3706f8c73cb5f63e22
hirodinn <hirebikila1@gmail.com>
2025-10-29 22:20:51 +0300

    finish the weather container

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,67 @@
-import { FootballNews } from "./FootballNews";
+import axios from "axios";
 import "./Home.css";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 export function Home() {
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+  const userInfo = useSelector((state) => state.userInfo);
+  const [weather, setWeather] = useState(null);
+  const date = new Date();
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${userInfo.city}&appid=${apiKey}&units=metric`
+        );
+        setWeather(response.data);
+      } catch (error) {
+        console.error("Error fetching weather:", error);
+      }
+    }
+    loadData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="home-container">
       <main>
         <header>
-          <h1>Good Morining, Hire</h1>
+          <h1>Good Morning, {userInfo.name}</h1>
           <button>Click</button>
         </header>
         <div className="grid-container">
-          <div className="clock"></div>
+          <div className="clock">
+            <div>
+              <h1 style={{ fontSize: "70px" }}>
+                {date.toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </h1>
+              <p>
+                {date.toLocaleDateString("en-US", {
+                  weekday: "long",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </p>
+            </div>
+            <div className="weather-container">
+              {weather && (
+                <>
+                  <div>
+                    <img
+                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                      alt="Weather Icon"
+                    />
+                    <h3>{Math.round(weather.main.temp)}°C</h3>
+                  </div>
+                  <p>{weather.weather[0].description}</p>
+                </>
+              )}
+            </div>
+          </div>
           <div className="weather"></div>
           <div className="news"></div>
           <div className="todo"></div>
--- src/redux/userInfoReducer.js
+++ src/redux/userInfoReducer.js
@@ -2,6 +2,7 @@ const initialValue = JSON.parse(localStorage.getItem("userInfo")) || {};
 export default function UserInfoReducer(state = initialValue, action) {
   if (action.type === "ADD") {
     const temp = { ...state, ...action.payload };
+    localStorage.setItem("userInfo", JSON.stringify(temp));
     return temp;
   } else {
     return state;
