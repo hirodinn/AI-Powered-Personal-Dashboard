6c35adeb8520991043550f440bb0b0a5f28335b4
hirodinn <hirebikila1@gmail.com>
2025-11-01 18:52:15 +0300

    add the humberger icon and style it

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,19 @@
 import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
+import { useState } from "react";
 
 export function Sidebar() {
+  const [expand, setExpand] = useState(true);
   return (
-    <nav className="sidebar-nav">
+    <nav className={`sidebar-nav ${expand && "expand-nav"}`}>
+      <button>
+        <i className="" />
+      </button>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/football-news">Football News</NavLink>
       <NavLink to="/weather">Weather</NavLink>
-      <NavLink to="/todo">Football News</NavLink>
-      <NavLink to="/gemini">Football News</NavLink>
+      <NavLink to="/gemini">Gemini</NavLink>
+      <NavLink to="/todo">Todo</NavLink>
     </nav>
   );
 }
