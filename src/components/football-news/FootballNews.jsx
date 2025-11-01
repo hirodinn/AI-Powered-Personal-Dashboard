a7766afc8474cb33dc9bd7103942a96aa5b28f6d
hirodinn <hirebikila1@gmail.com>
2025-11-01 19:15:10 +0300

    add the sidebar to the football news component

--- src/components/football-news/FootballNews.jsx
+++ src/components/football-news/FootballNews.jsx
@@ -1,22 +1,26 @@
+import { Sidebar } from "../sidebar/Sidebar";
 import "./FootballNews.css";
 
 export function FootballNews({ news }) {
   return (
-    <div className="football-news">
-      <h1>Football news</h1>
-      {news &&
-        news.map((article, i) => {
-          return (
-            <article key={i}>
-              <img src={article.urlToImage} />
-              <div className="info-container">
-                <p className="title">{article.title}</p>
-                <p className="source">{article.source.name}</p>
-                <p className="description">{article.description}</p>
-              </div>
-            </article>
-          );
-        })}
+    <div className="football-news-container">
+      <Sidebar />
+      <div className="football-news">
+        <h1>Football news</h1>
+        {news &&
+          news.map((article, i) => {
+            return (
+              <article key={i}>
+                <img src={article.urlToImage} />
+                <div className="info-container">
+                  <p className="title">{article.title}</p>
+                  <p className="source">{article.source.name}</p>
+                  <p className="description">{article.description}</p>
+                </div>
+              </article>
+            );
+          })}
+      </div>
     </div>
   );
 }
