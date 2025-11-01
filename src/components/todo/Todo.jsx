dd48b47a85501dbc62e720a1b5f2b99e2bf89526
hirodinn <hirebikila1@gmail.com>
2025-11-01 19:19:44 +0300

    add the sidebar to the todo container

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -2,6 +2,7 @@ import "./Todo.css";
 import { EachTodo } from "./EachTodo";
 import { useEffect, useState, useRef } from "react";
 import { current } from "@reduxjs/toolkit";
+import { Sidebar } from "../sidebar/Sidebar";
 export function Todo() {
   const [todo, setTodo] = useState(
     JSON.parse(localStorage.getItem("todo")) || []
@@ -24,22 +25,25 @@ export function Todo() {
     if (current) containerElem.scrollTop = containerElem.scrollHeight;
   }, [todoRedux]);
   return (
-    <div className="todo-container">
-      <form onSubmit={addEntry}>
-        <input
-          type="text"
-          placeholder="Add new Todo List"
-          value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-          }}
-        />
-        <button>Submit</button>
-      </form>
-      <div className="todo-lists" ref={containerRef}>
-        {todoRedux.map((each, i) => {
-          return <EachTodo todo={todo} each={each} key={i} />;
-        })}
+    <div className="todo-component">
+      <Sidebar />
+      <div className="todo-container">
+        <form onSubmit={addEntry}>
+          <input
+            type="text"
+            placeholder="Add new Todo List"
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.target.value);
+            }}
+          />
+          <button>Submit</button>
+        </form>
+        <div className="todo-lists" ref={containerRef}>
+          {todoRedux.map((each, i) => {
+            return <EachTodo todo={todo} each={each} key={i} />;
+          })}
+        </div>
       </div>
     </div>
   );
